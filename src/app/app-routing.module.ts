e54ebd8fb081a49917773c38fd80e7b1ad7621c7
K-murielle <murielle.keudjeu@odoolink.io>
2025-01-23 20:13:41 +0100

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,20 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'loader',
+    loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
 ];
 
