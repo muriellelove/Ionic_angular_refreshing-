29f57c479c01ac5a23d35e266167fa1ccba3e453
muriellelove <keudjeu.murielle@gmail.com>
2025-01-28 17:08:44 +0100

    add pickup call page and update styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'pickup-call',
+    loadChildren: () => import('./pages/pickup-call/pickup-call.module').then( m => m.PickupCallPageModule)
+  },
 ];
 
 @NgModule({
